08c9dae2331766a8a31187e2ef3459c71acedd52
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-07-14 16:23:48 +0530

    add getcookies

--- attend/test.php
+++ attend/test.php
@@ -58,7 +58,7 @@ time_spent
 -->
 
 <script>
-window.onload = checkCookie();
+checkCookie();
 
 function checkCookie() {
     let user = getCookie("user");
@@ -73,6 +73,22 @@ function checkCookie() {
        }
     }
   }
+
+  function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
   
 function setCookie(cname,cvalue,exdays) {
   const d = new Date();
