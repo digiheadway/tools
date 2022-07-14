9ad654160fe19aa591249d83bb8094a3e30356da
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-07-14 17:00:03 +0530

    setcookies fix

--- attend/setcookies.php
+++ attend/setcookies.php
@@ -4,7 +4,7 @@ $user = $_GET['user'];
 
 if($user){
     setcookie("user", $user, time() + (86400 * 365), "/"); // 86400 = 1 day
- //   header('Location: index.php');
+    header('Location: index.php');
 }else{
     echo "No User Info Found";
 }
