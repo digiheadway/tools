c1e6425aeebb2d167c3e50aacbe37214ac1d5434
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-15 16:55:01 +0530

    as

--- attend/function.php
+++ attend/function.php
@@ -22,7 +22,7 @@ function fetch_data($user)
     var_dump($result);
     echo $sql;
     if ($result) {
-        $rows = $result->fetch_array(MYSQL_ASSOC);
+        $rows = $result->fetch_all();
         var_dump($rows);
         $conn->close();
         return $rows;
