37595d043405ffbc90dbbf0c1258e41f1210f1fe
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-15 16:01:50 +0530

    ds

--- attend/function.php
+++ attend/function.php
@@ -23,6 +23,7 @@ function fetch_data()
 
     $sql = "SELECT date, sum(time_spent) as total_hours FROM attend WHERE name='" . $user . "' GROUP BY date";
     $result = $conn->query($sql);
+    echo $sql;
     if ($result) {
         $rows = $result->fetch_all(MYSQL_ASSOC);
         $conn->close();
