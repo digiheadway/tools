30c3c26a911134d0e3090c92c767777bc6186d52
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-18 11:06:24 +0530

    Printing today hours

--- attend/index.php
+++ attend/index.php
@@ -5,14 +5,23 @@ var_dump($user);
 $hour_rows = "";
 $half_days = 0;
 $full_days = 0;
+$today_hours = 0;
+$today = date("Y-m-d");
 foreach ($user['data'] as $row) {
     $total_hours = intval($row['total_hours']);
     $hour_rows .= "<tr><td>{$row['date']}</td><td>$total_hours Hours</td></tr>";
+    
+    // Calculating half & full days
     if($total_hours >= 8){
         $full_days++;
     }else{
         $half_days++;
     }
+
+    if($row['date'] == $today){
+        $today_hours = $total_hours;
+    }
+
   }
 
 ?>
@@ -36,7 +45,7 @@ foreach ($user['data'] as $row) {
 
             <section class="first_section">
 
-                <p>Today You Spent: 0 Hour in Office</p>
+                <p>Today You Spent: <? echo $today_hours  ?> Hour in Office</p>
                 <button>Chek in </button>
                 <div class="grop_contant">
                     <p><b>This Month</b></p>
