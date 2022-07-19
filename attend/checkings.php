2333730208fc36b867572bf14641b007bed15116
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-19 12:00:21 +0530

    ds

--- attend/checkings.php
+++ attend/checkings.php
@@ -1,6 +1,7 @@
 <?php
 
-date_default_timezone_set("Asia/Calcutta"); 
+date_default_timezone_set("Asia/Calcutta");
+
 
 
 function process_query($query)
@@ -25,16 +26,17 @@ function main($user_name)
     $data = json_decode(trim(file_get_contents("php://input")), true);
     $lat = $data["lat"];
     $lon = $data["lon"];
+    $location = "ST_GeomFromText('POINT($lat,  $lon)')";
 
     if ($data["action"] == "checkIn") {
         $today = date("Y-m-d");
 
-        $query = "INSERT INTO attend (name, date, checkin_time, checkin_loc, checkin_ip) VALUES ('$user_name','$today', '$time', POINT($lat,  $lon), '$ip')";
+        $query = "INSERT INTO attend (name, date, checkin_time, checkin_loc, checkin_ip) VALUES ('$user_name','$today', '$time', $location, '$ip')";
         process_query($query);
     }
     else if ($data["action"] == "checkOut") {
         $id = $data["id"];
-        $query = "UPDATE attend SET checkout_time = '$time', checkout_ip = '$ip', checkout_loc = POINT($lat, $lon) WHERE id = $id";
+        $query = "UPDATE attend SET checkout_time = '$time', checkout_ip = '$ip', checkout_loc = $location WHERE id = $id";
         process_query($query);
     }
 }
