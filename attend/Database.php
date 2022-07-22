efbce563f78ac52170d9c245bd97d0de26e226a4
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-22 14:02:35 +0530

    Updated Database Class

--- attend/Database.php
+++ attend/Database.php
@@ -38,6 +38,17 @@ class Database
         return $rows;
     }
 
+    function insert(string $table, array $columns, string $value_str){
+        $columns = join($columns, ", ");
+        $query = "INSERT INTO $table ($columns) VALUES ($value_str)";
+        return $this->process_query($query);
+    }
+
+    function update(string $table, string $set_str, $id){
+        $query = "UPDATE $table SET $set_str WHERE id = $id";
+        return $this->process_query($query);
+    }
+
     function close()
     {
         $this->conn->close();
--- attend/checkings.php
+++ attend/checkings.php
@@ -11,15 +11,6 @@ function notify($username, $action)
     file_get_contents($url);
 }
 
-function process_query($query)
-{
-    include_once("Database.php");
-    $db = new Database();
-    $db->process_query($query);
-    $db->close();
-
-}
-
 $user_name = $_COOKIE['user'];
 if (!$user_name) {
     echo json_encode(['res' => 'error']);
@@ -28,6 +19,9 @@ if (!$user_name) {
 
 function main($user_name)
 {
+    include_once("Database.php");
+    $db = new Database();
+
     $time = date("H:i:s");
     $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
     $data = json_decode(trim(file_get_contents("php://input")), true);
@@ -37,18 +31,19 @@ function main($user_name)
     $action = "checked in";
     if ($data["action"] == "checkIn") {
         $today = date("Y-m-d");
-        
-        $query = "INSERT INTO attend (name, date, checkin_time, checkin_loc, checkin_ip) VALUES ('$user_name','$today', '$time', $location, '$ip')";
-        process_query($query);
+
+        $value_str = "'$user_name','$today', '$time', $location, '$ip'";
+        $db->insert("attend", ["name", "date", "checkin_time", "checkin_loc", "checkin_ip"], $value_str);
     }
     else if ($data["action"] == "checkOut") {
         $action = "checked out";
-        $id = $data["id"];
-        $query = "UPDATE attend SET checkout_time = '$time', checkout_ip = '$ip', checkout_loc = $location WHERE id = $id";
-        process_query($query);
+
+        $set_str = "checkout_time = '$time', checkout_ip = '$ip', checkout_loc = $location";
+        $db->update("attend", $set_str, $data["id"]);
     }
 
-    notify($user_name, $action );
+    $db->close();
+    notify($user_name, $action);
 }
 
 try {
