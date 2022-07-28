3b4bc01eba8aeeaea8e4166ba352bff247432f58
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-07-29 01:40:54 +0530

    css

--- attend/index.php
+++ attend/index.php
@@ -40,7 +40,7 @@ foreach ($user['hours_spent'] as $row) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Attandance Fetch Data</title>
-    <link rel="stylesheet" href="style.css?v=0.0.1">                
+    <link rel="stylesheet" href="style.css?v=0.0.2">                
 </head>
 <body>
     <div class="container">
