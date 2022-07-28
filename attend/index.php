166105ff0d1ce6191ac0596cae8cbfe95ba30391
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-07-29 03:52:59 +0530

    pwa

--- attend/index.php
+++ attend/index.php
@@ -39,7 +39,7 @@ foreach ($user['hours_spent'] as $row) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attandance Fetch Data</title>
+    <title>Time Tracker</title>
     <link rel="stylesheet" href="style.css?v=0.0.6">        
     <meta http-equiv="refresh" content="300">
         
