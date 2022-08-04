72bbb4a42c450c89e2f028258cec32bbd82f352f
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-08-04 12:35:37 +0530

    sfd

--- restore.php
+++ restore.php
@@ -5,8 +5,8 @@ $password = "#DNhomg$:p7L";
 $database = "u240376517_tools";
 $filepath = "backup.sql";
 
-// exec("mysqldump --user=$user --password=$password --host=$host  $database  < $targetFifilepathlePath");
-exec("mysql --user=$user --password=$password -host=$host $database < $filepath");
+exec("mysqldump --user=$user --password=$password --host=$host  $database  < $targetFifilepathlePath");
+// exec("mysql --user=$user --password=$password -host=$host $database < $filepath");
 // exec("mysqlimport --user $user --password $password --host $host $database $filepath");
 
 // mysql -u [user] -p [database_name] < [filename].sql
